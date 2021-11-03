24f35b46f4deb7e2ce7ebcefa1d22e7306d8fe60
Zakhar Skyba <68905247+zakkiakafaka@users.noreply.github.com>
2021-11-03 23:32:47 +0200

    Updates

---
+++ static/js/main.74d5a1e8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpinfopartstest=this.webpackJsonpinfopartstest||[]).push([[0],[function(t,s,n){"use strict";n.r(s);n(1)},function(t,s,n){}],[[0,1]]]);
+//# sourceMappingURL=main.74d5a1e8.chunk.js.map
